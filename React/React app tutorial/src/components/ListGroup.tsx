1ad1aca5aa773a79de4fe15e6c7ac2f7ffacff4b
Joshua <joshualai2003@gmail.com>
2026-09-25 14:02:15 +0800

    react for loop and item key for future references

--- React/React app tutorial/src/components/ListGroup.tsx
+++ React/React app tutorial/src/components/ListGroup.tsx
@@ -1,20 +1,24 @@
 //fragment is better than div
+//can import fragment from react or also just use empty <>
 
 function ListGroup() {
+  const items = ["New York", "London", "Tokyo", "San Francisco"];
+
   return (
-    <>   
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((items) => (
+          <li key={items}>{items}</li>
+        ))}
+      </ul>
     </>
   );
 }
 //ctrl D after you select one item to select multiple of that element
 //in react component cannot return more than one element
+
+//using items.map -> jsx way of using a for loop but react will throw us an error
+//so we wrap them in {} 
 
 export default ListGroup;
