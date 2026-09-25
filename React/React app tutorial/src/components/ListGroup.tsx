06e533d2a3ffccb37d84a7bc56fb322bf6b2abc1
Joshua <joshualai2003@gmail.com>
2026-09-25 13:55:05 +0800

    Using fragments to return more than 1 item

--- React/React app tutorial/src/components/ListGroup.tsx
+++ React/React app tutorial/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+//fragment is better than div
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>   
+        <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
+    </>
   );
 }
 //ctrl D after you select one item to select multiple of that element
+//in react component cannot return more than one element
 
 export default ListGroup;
