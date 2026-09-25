8395871ac642a132a08b0490bacd3370ca8839db
Joshua <joshualai2003@gmail.com>
2026-09-25 13:29:42 +0800

    Initial Changes

---
+++ React/React app tutorial/src/Message.tsx
@@ -0,0 +1,16 @@
+//Creating a message component using Typescript
+
+//pascal casing when creating function components
+function Message(){
+//describing what should be inside a H1 element with Hello World
+    //code gets converted to Javascript
+    return <h1>Hello World</h1>;
+}
+
+export default Message
+
+
+
+
+
+
