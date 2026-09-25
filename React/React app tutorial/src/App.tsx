0325c405956073f7d58bc894f2527b0e91f319c0
Joshua <joshualai2003@gmail.com>
2026-09-25 13:49:26 +0800

    more changes using bootstrap

--- React/React app tutorial/src/App.tsx
+++ React/React app tutorial/src/App.tsx
@@ -1,7 +1,7 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup"
 
 function App(){  
-  return <div><Message /></div>
+  return <div><ListGroup /></div>
 }
 
 //export app so it can be used somewhere else
