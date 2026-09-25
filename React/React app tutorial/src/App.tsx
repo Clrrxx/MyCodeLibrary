4164a818c3662b7d7f9439e1adbb1429a56e9e56
Joshua <joshualai2003@gmail.com>
2026-09-25 13:37:37 +0800

    More changes

--- React/React app tutorial/src/App.tsx
+++ React/React app tutorial/src/App.tsx
@@ -1,6 +1,6 @@
 import Message from "./Message";
 
-function App(){
+function App(){  
   return <div><Message /></div>
 }
 
--- React/React app tutorial/src/Message.tsx
+++ React/React app tutorial/src/Message.tsx
@@ -4,6 +4,10 @@
 function Message(){
 //describing what should be inside a H1 element with Hello World
     //code gets converted to Javascript
+    //using JSX => we can do dynamic content
+    const name = 'Josh';
+    if (name) 
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
 }
 
